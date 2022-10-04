f52596cbfcd16f997f6646b5acfb9fcafa670b6a
josef <josef@cubiclease.com>
2022-10-04 22:44:44 +0100

    Added open weather config

---
+++ config/open_weather.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'forecast-endpoint'  => 'api.openweathermap.org/data/2.5/forecast',
+    'geocoding-endpoint' => 'http://api.openweathermap.org/geo/1.0/direct',
+    'key'                => env('OPEN_WEATHER_KEY'),
+];
