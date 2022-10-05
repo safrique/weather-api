37ea03641f671ead0a99f25b39e8da80e32e50a0
josef <josef@cubiclease.com>
2022-10-05 23:17:59 +0100

    Added service to store a city

---
+++ app/Services/Cities/StoreCityService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\Cities;
+
+use App\Models\City;
+use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+class StoreCityService
+{
+    /**
+     * @param array $data
+     *
+     * @return array|string
+     */
+    public function store(array $data)
+    {
+        try {
+            return is_string($validate = $this->validate($data)) ? $validate : $this->save($validate);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array|string
+     * @throws BindingResolutionException
+     */
+    private function validate(array $data)
+    {
+        foreach (City::getFillable() as $item) {
+            if (empty($data[$item])) {
+                return "Missing required $item parameter";
+            }
+
+            $return_data[$item] = $data[$item];
+        }
+
+        return $return_data ?? [];
+    }
+
+    private function save(array $data)
+    : array {
+        return City::updateOrCreate($data)->toArray();
+    }
+}
