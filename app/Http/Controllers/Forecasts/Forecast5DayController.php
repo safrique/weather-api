7996310a4c9a36c750c0f5a4c6914582e78df9aa
josef <josef@cubiclease.com>
2022-10-06 00:06:38 +0100

    Added 5-day forecast controller

---
+++ app/Http/Controllers/Forecasts/Forecast5DayController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Forecasts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Forecast5DayController extends Controller
+{
+    //
+}
