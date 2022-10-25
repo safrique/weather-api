6301840f9058a5144d472dfe87af810aeab81f40
josef <josef@cubiclease.com>
2022-10-25 09:59:35 +0100

    Removed take-home test text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,5 @@
 @section('content')
     <h3>Welcome</h3>
 
-    Welcome to my take-home coding project. Please follow the instructions on the README.md file and use the
-    navigation menu above to navigate around the site.
+    Please follow the instructions on the README.md file and use the navigation menu above to navigate around the site.
 @endsection
